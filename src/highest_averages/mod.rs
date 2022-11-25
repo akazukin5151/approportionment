8a0675aa1b50fdbf42164cb674bfb22bab19130e
akazukin5151 <tsuiyikching@protonmail.com>
2022-11-25 23:34:43 +0000

    Add webster/sainte lague

--- src/highest_averages/mod.rs
+++ src/highest_averages/mod.rs
@@ -1,5 +1,7 @@
 pub mod lib;
 pub mod dhondt;
+pub mod webster;
 
 pub use lib::*;
 pub use dhondt::*;
+pub use webster::*;
---
+++ src/highest_averages/webster.rs
@@ -0,0 +1,34 @@
+use crate::*;
+
+struct WebsterSainteLague;
+
+impl HighestAverages for WebsterSainteLague {
+    fn quotient(original_votes: u64, n_seats_won: u32) -> u64 {
+        original_votes / (2 * n_seats_won as u64 + 1)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_webster_sainte_lague_wikipedia() {
+        let a = generic_party("A");
+        let b = generic_party("B");
+        let c = generic_party("C");
+        let d = generic_party("D");
+
+        let mut ballots = vec![a; 10_000];
+        ballots.extend(vec![b; 8_000]);
+        ballots.extend(vec![c; 3_000]);
+        ballots.extend(vec![d; 2_000]);
+
+        let r = WebsterSainteLague::allocate_seats(8, &ballots);
+
+        assert(&r, "A", 3);
+        assert(&r, "B", 3);
+        assert(&r, "C", 1);
+        assert(&r, "D", 1);
+    }
+}
