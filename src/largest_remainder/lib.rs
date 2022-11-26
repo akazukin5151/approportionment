d09d23710e5d78a950fc4411724c8c8501432fa3
akazukin5151 <tsuiyikching@protonmail.com>
2022-11-26 01:56:43 +0000

    Improve largest remainder code

--- src/largest_remainder/lib.rs
+++ src/largest_remainder/lib.rs
@@ -18,16 +18,14 @@ pub fn allocate_largest_remainder(
     let counts_divided_by_quota =
         counts.iter().map(|(p, x)| (p, *x as f64 / quota as f64));
 
-    let automatic_seats_and_remainder =
-        counts_divided_by_quota.map(|(p, x)| {
-            let automatic_seats = x.floor();
-            let remainder = x - automatic_seats;
-            (p, automatic_seats as u32, remainder)
-        });
-    let automatic_seats = automatic_seats_and_remainder
-        .clone()
-        .map(|(p, x, _)| (p, x));
-    let remainders = automatic_seats_and_remainder.map(|(p, _, x)| (p, x));
+    let mut automatic_seats = vec![];
+    let mut remainders = vec![];
+    for (p, x) in counts_divided_by_quota {
+        let as_ = x.floor();
+        let remainder = x - as_;
+        automatic_seats.push((p, as_ as u32));
+        remainders.push((p, remainder));
+    }
 
     let mut result: HashMap<Party, u32> = HashMap::new();
     let mut remaining_n_seats = total_seats;
@@ -36,19 +34,18 @@ pub fn allocate_largest_remainder(
         remaining_n_seats -= x;
     }
 
-    let mut remainders_sorted: Vec<_> = remainders.collect();
-    remainders_sorted
-        .sort_by_key(|(_, x)| unsafe { NotNan::new_unchecked(*x) });
-    let highest_remainder_seats = remainders_sorted
-        .iter()
-        .rev()
-        .take(remaining_n_seats as usize);
+    // TODO(perf): optimize for only taking highest X amount instead of sorting all
+    remainders.sort_by_key(|(_, x)| NotNan::new(*x).unwrap());
+    let highest_remainder_seats =
+        remainders.iter().rev().take(remaining_n_seats as usize);
     let highest_remainder_parties = highest_remainder_seats.map(|(p, _)| p);
+
     for party in highest_remainder_parties {
         result
             .entry((*party).clone())
             .and_modify(|s| *s += 1)
             .or_insert(1);
     }
+
     result
 }
