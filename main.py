c8bddf4f67bb7f9ff510952c411c0327423a0724
akazukin5151 <tsuiyikching@protonmail.com>
2022-12-05 11:46:33 +0000

    Rearrange order of functions

--- main.py
+++ main.py
@@ -8,61 +8,14 @@ import matplotlib as mpl
 import seaborn as sns
 import dhall
 
-def setup_subplots(is_discrete):
-    if not is_discrete:
-        return plt.subplots(ncols=2, figsize=(7, 5), width_ratios=[20, 1])
-    fig, ax = plt.subplots(figsize=(7, 5))
-    return fig, [ax]
+def main():
+    with open('config.dhall', 'r') as f:
+        configs = dhall.load(f)
 
-def plot_seats(df_for_party, palette, axes):
-    sns.scatterplot(
-        data=df_for_party,
-        x='x',
-        y='y',
-        hue='seats_for_party',
-        palette=palette,
-        s=2,
-        legend=None,
-        ax=axes[0]
-    )
-    axes[0].axis('off')
-
-def plot_cbar_or_legend(df, fig, axes, palette, is_discrete):
-    s = df.seats_for_party
-    if len(axes) == 2:
-        matrix = [range(s.min(), s.max() + 1)]
-        psm = axes[1].pcolormesh(
-            matrix,
-            cmap=palette,
-            rasterized=True
-        )
-        fig.colorbar(psm, cax=axes[1])
-    elif is_discrete:
-        colors = mpl.colormaps[palette].colors
-        artists = [Circle((0, 0), 1, color=c) for c in colors]
-        axes[0].legend(artists, range(s.max() + 1))
-
-def plot_parties(parties, axes):
-    df = pd.DataFrame(parties)
-    palette = df['color'].apply(
-        lambda x: (x['r'] / 255, x['g'] / 255, x['b'] / 255)
-    ).to_list()
-    sns.scatterplot(
-        data=df,
-        x='x',
-        y='y',
-        hue='name',
-        s=50,
-        ax=axes[0],
-        legend=False,
-        palette=palette
-    )
-
-def format_plot(axes):
-    for ax in axes:
-        ax.margins(0.01)
-
-    plt.tight_layout()
+    for config in configs:
+        path = Path(config['data_out_dir'])
+        for file in os.listdir(path):
+            plot_all(config, path, file)
 
 def plot_all(config, path, file):
     df = pd.read_feather(path / file)
@@ -122,14 +75,61 @@ def plot_colorscheme(
     path.parent.mkdir(exist_ok=True, parents=True)
     plt.savefig(path)
 
-def main():
-    with open('config.dhall', 'r') as f:
-        configs = dhall.load(f)
+def setup_subplots(is_discrete):
+    if not is_discrete:
+        return plt.subplots(ncols=2, figsize=(7, 5), width_ratios=[20, 1])
+    fig, ax = plt.subplots(figsize=(7, 5))
+    return fig, [ax]
 
-    for config in configs:
-        path = Path(config['data_out_dir'])
-        for file in os.listdir(path):
-            plot_all(config, path, file)
+def plot_seats(df_for_party, palette, axes):
+    sns.scatterplot(
+        data=df_for_party,
+        x='x',
+        y='y',
+        hue='seats_for_party',
+        palette=palette,
+        s=2,
+        legend=None,
+        ax=axes[0]
+    )
+    axes[0].axis('off')
+
+def plot_cbar_or_legend(df, fig, axes, palette, is_discrete):
+    s = df.seats_for_party
+    if len(axes) == 2:
+        matrix = [range(s.min(), s.max() + 1)]
+        psm = axes[1].pcolormesh(
+            matrix,
+            cmap=palette,
+            rasterized=True
+        )
+        fig.colorbar(psm, cax=axes[1])
+    elif is_discrete:
+        colors = mpl.colormaps[palette].colors
+        artists = [Circle((0, 0), 1, color=c) for c in colors]
+        axes[0].legend(artists, range(s.max() + 1))
+
+def plot_parties(parties, axes):
+    df = pd.DataFrame(parties)
+    palette = df['color'].apply(
+        lambda x: (x['r'] / 255, x['g'] / 255, x['b'] / 255)
+    ).to_list()
+    sns.scatterplot(
+        data=df,
+        x='x',
+        y='y',
+        hue='name',
+        s=50,
+        ax=axes[0],
+        legend=False,
+        palette=palette
+    )
+
+def format_plot(axes):
+    for ax in axes:
+        ax.margins(0.01)
+
+    plt.tight_layout()
 
 
 if __name__ == '__main__':
