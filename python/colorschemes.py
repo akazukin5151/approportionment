5ed14f74a5ee7c1ec20e061330b016f6cfad93c9
akazukin5151 <tsuiyikching@protonmail.com>
2022-12-07 17:00:48 +0000

    Add type hints to colorschemes.py

--- python/colorschemes.py
+++ python/colorschemes.py
@@ -1,42 +1,87 @@
-from abc import ABC
+from __future__ import annotations
+from typing import Union, TYPE_CHECKING
+from abc import ABC, abstractmethod
 from matplotlib.patches import Circle
 import matplotlib as mpl
 
+if TYPE_CHECKING:
+    import pandas as pd
+    import numpy as np
+    import numpy.typing as npt
+
 class Colorscheme(ABC):
     """Interface that every colorscheme must implement"""
-    def get_party_to_colorize(p, parties):
+    @staticmethod
+    @abstractmethod
+    def get_party_to_colorize(
+        p: dict[str, str],
+        parties: list[dict[str, str]]
+    ) -> dict[str, str]:
         """Extract party_to_colorize"""
 
-    def get_cmap(p, df, total_seats):
+    @staticmethod
+    @abstractmethod
+    def get_cmap(
+        p: dict[str, str],
+        df: pd.DataFrame,
+        total_seats: int
+    ) -> Union[str, list[list[float]]]:
         """Calculate cmap and calculate the color column for the df"""
 
-    def legend_items(palette, unique_seats):
+    @staticmethod
+    @abstractmethod
+    def legend_items(
+        palette: Union[str, list[list[float]]],
+        unique_seats: npt.NDArray[np.int_]
+    ) -> list[Circle]:
         """Calculate the legend items"""
 
 class Majority(Colorscheme):
-    def get_party_to_colorize(p, parties):
+    @staticmethod
+    def get_party_to_colorize(
+        p: dict[str, str],
+        parties: list[dict[str, str]]
+    ) -> dict[str, str]:
         return find_pc(parties, p['for_party'])
 
-    def get_cmap(_, df, total_seats):
+    @staticmethod
+    def get_cmap(
+        _: dict[str, str],
+        df: pd.DataFrame,
+        total_seats: int
+    ) -> Union[str, list[list[float]]]:
         df['seats_for_party'] = (
             (df['seats_for_party'] / total_seats) >= 0.5
         ).astype(int)
         red = mpl.colormaps['tab10'](3)
         green = mpl.colormaps['tab10'](2)
-        cmap = [red, green]
+        cmap: list[list[float]] = [red, green]
         df['color'] = df['seats_for_party'].apply(
             lambda m: cmap[0] if m == 0 else cmap[1]
         )
         return cmap
 
-    def legend_items(palette, _):
+    @staticmethod
+    def legend_items(
+        palette: Union[str, list[list[float]]],
+        _: npt.NDArray[np.int_]
+    ) -> list[Circle]:
         return [Circle((0, 0), 1, color=c) for c in palette]
 
 class Discrete(Colorscheme):
-    def get_party_to_colorize(p, parties):
+    @staticmethod
+    def get_party_to_colorize(
+        p: dict[str, str],
+        parties: list[dict[str, str]]
+    ) -> dict[str, str]:
         return find_pc(parties, p['party_to_colorize'])
 
-    def get_cmap(p, df_for_party, total_seats):
+    @staticmethod
+    def get_cmap(
+        p: dict[str, str],
+        df_for_party: pd.DataFrame,
+        total_seats: int
+    ) -> Union[str, list[list[float]]]:
         cmap = p['palette_name']
         mapper = mpl.colormaps[cmap]
         # if this is a continuous colormap, resample it
@@ -46,14 +91,18 @@ class Discrete(Colorscheme):
         df_for_party['color'] = df_for_party['seats_for_party'].apply(mapper)
         return cmap
 
-    def legend_items(palette, unique_seats):
+    @staticmethod
+    def legend_items(
+        palette: Union[str, list[list[float]]],
+        unique_seats: npt.NDArray[np.int_]
+    ) -> list[Circle]:
         colors = mpl.colormaps[palette]
         # if this is a continuous colormap, resample it
         if colors.N > 15:
             colors = colors.resampled(unique_seats.size)
         return [Circle((0, 0), 1, color=colors(i)) for i in unique_seats]
 
-def find_pc(parties, name):
+def find_pc(parties: list[dict[str, str]], name: str) -> dict[str, str]:
     return [
         party
         for party in parties
