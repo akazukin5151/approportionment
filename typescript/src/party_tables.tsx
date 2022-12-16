91ba378dae0b06f798dcfd71f8118d51aa9c2622
akazukin5151 <tsuiyikching@protonmail.com>
2022-12-17 02:30:19 +0800

    Fix bug where multiple radios might be selected

--- typescript/src/party_tables.tsx
+++ typescript/src/party_tables.tsx
@@ -18,6 +18,65 @@ function random_color() {
   return d3.rgb(r, g, b)
 }
 
+function generic_new_row(
+  tbody: HTMLTableSectionElement,
+  check_radio: boolean,
+  given_color: string,
+  x: number,
+  y: number
+) {
+  const row = document.createElement('tr')
+
+  const input = document.createElement('input')
+  input.setAttribute('type', "radio")
+  input.setAttribute('class', 'party_radio')
+  input.setAttribute('name', 'party_radio')
+  if (check_radio) {
+    input.checked = true
+  }
+  const td1 = document.createElement('td')
+  td1.appendChild(input)
+  row.appendChild(td1)
+
+  const numbers = Array.from(tbody.children)
+    .filter((_, idx) => idx !== 0)
+    .map(row => {
+      const td = row.children[1] as HTMLElement
+      return parseInt(td.innerText)
+    })
+  // Ensure there is at least one item in the array
+  numbers.push(0)
+  const max_party_num = Math.max(...numbers)
+  const next_party_num = max_party_num + 1
+
+  const td2 = document.createElement('td')
+  td2.appendChild(document.createTextNode(next_party_num.toString()))
+  row.appendChild(td2)
+
+  const color = document.createElement('input')
+  color.setAttribute('type', "color")
+  color.value = given_color
+  const td3 = document.createElement('td')
+  td3.appendChild(color)
+  row.appendChild(td3)
+
+  const td4 = document.createElement('td')
+  td4.appendChild(document.createTextNode(x.toString()))
+  row.appendChild(td4)
+
+  const td5 = document.createElement('td')
+  td5.appendChild(document.createTextNode(y.toString()))
+  row.appendChild(td5)
+
+  const delete_btn = document.createElement('button')
+  delete_btn.innerText = 'Delete'
+  delete_btn.onclick = delete_party
+  row.appendChild(delete_btn)
+
+  tbody.appendChild(row)
+  return next_party_num
+}
+
 export function setup_party_buttons(setup: Setup) {
   const table = document.getElementById('party_table')
   if (!table) { return }
@@ -27,57 +86,16 @@ export function setup_party_buttons(setup: Setup) {
 
   const btn = document.getElementById('add_party_button')
   btn?.addEventListener("click", () => {
-    const row = document.createElement('tr')
-
-    const input = document.createElement('input')
-    input.setAttribute('type', "radio")
-    const td1 = document.createElement('td')
-    td1.appendChild(input)
-    row.appendChild(td1)
-
-    const numbers = Array.from(tbody.children)
-      .filter((_, idx) => idx !== 0)
-      .map(row => {
-        const td = row.children[1] as HTMLElement
-        return parseInt(td.innerText)
-      })
-    // Ensure there is at least one item in the array
-    numbers.push(0)
-    const max_party_num = Math.max(...numbers)
-    const next_party_num = max_party_num + 1
-
-    const td2 = document.createElement('td')
-    td2.appendChild(document.createTextNode(next_party_num.toString()))
-    row.appendChild(td2)
-
-    const color = document.createElement('input')
-    color.setAttribute('type', "color")
-    color.value = random_color().formatHex()
-    const td3 = document.createElement('td')
-    td3.appendChild(color)
-    row.appendChild(td3)
-
+    const color = random_color().formatHex()
     const x = round_1dp(random_between(-1, 1))
-    const td4 = document.createElement('td')
-    td4.appendChild(document.createTextNode(x.toString()))
-    row.appendChild(td4)
-
     const y = round_1dp(random_between(-1, 1))
-    const td5 = document.createElement('td')
-    td5.appendChild(document.createTextNode(y.toString()))
-    row.appendChild(td5)
 
-    const delete_btn = document.createElement('button')
-    delete_btn.innerText = 'Delete'
-    delete_btn.onclick = delete_party
-    row.appendChild(delete_btn)
-
-    tbody.appendChild(row)
+    const next_party_num = generic_new_row(tbody, false, color, x, y)
 
     const parties = [{
       x: x_scale(x),
       y: y_scale(y),
-      color: color.value,
+      color,
       num: next_party_num
     }]
     plot_party_core(setup, parties)
@@ -86,43 +104,7 @@ export function setup_party_buttons(setup: Setup) {
 
 function add_default_parties(tbody: HTMLTableSectionElement) {
   DEFAULT_PARTIES.forEach((party, idx) => {
-    const row = document.createElement('tr')
-
-    const input = document.createElement('input')
-    input.setAttribute('type', "radio")
-    input.name = "party"
-    input.setAttribute('class', "party_radio")
-    if (idx === 2) {
-      input.checked = true
-    }
-    const td0 = document.createElement('td')
-    td0.appendChild(input)
-    row.appendChild(td0)
-
-    const td1 = document.createElement('td')
-    td1.appendChild(document.createTextNode(idx.toString()))
-    row.appendChild(td1)
-
-    const color = document.createElement('input')
-    color.setAttribute('type', "color")
-    color.value = party.color
-    const td2 = document.createElement('td')
-    td2.appendChild(color)
-    row.appendChild(td2)
-
-    const td3 = document.createElement('td')
-    td3.appendChild(document.createTextNode(party.x.toString()))
-    row.appendChild(td3)
-
-    const td4 = document.createElement('td')
-    td4.appendChild(document.createTextNode(party.y.toString()))
-    row.appendChild(td4)
-
-    const delete_btn = document.createElement('button')
-    delete_btn.innerText = 'Delete'
-    delete_btn.onclick = delete_party
-    row.appendChild(delete_btn)
-    tbody.appendChild(row)
+    generic_new_row(tbody, idx === 2, party.color, party.x, party.y)
   })
 }
 
