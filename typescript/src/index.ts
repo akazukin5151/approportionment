1e696c06bf60814b6d5e411df98f4e3be5d75765
akazukin5151 <tsuiyikching@protonmail.com>
2022-12-16 03:29:13 +0000

    Enable simulation to be ran based on position of party points

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -21,7 +21,21 @@ function drag_ended(this: BaseType | SVGCircleElement) {
   d3.select(this).attr("stroke", null);
 }
 
-function load_parties(): Array<Circle> {
+function load_parties(
+  x_scale: d3.ScaleLinear<number, number, never>,
+  y_scale: d3.ScaleLinear<number, number, never>
+): Array<Circle> {
+  const elems = document.getElementsByClassName('party-circle');
+  if (elems && elems.length !== 0) {
+    return Array.from(elems).map((elem) => {
+      const cx = parseFloat(elem.getAttribute('cx') ?? '0')
+      const cy = parseFloat(elem.getAttribute('cy') ?? '0')
+      const x = x_scale.invert(cx)
+      const y = y_scale.invert(cy)
+      const color = elem.getAttribute('fill') ?? 'red'
+      return { x, y, color: color }
+    })
+  }
   return [
     { x: -0.7, y: 0.7, color: 'red' },
     { x: 0.7, y: 0.7, color: 'red' },
@@ -56,13 +70,35 @@ function main() {
   const party_to_colorize = 3;
   const cmap = d3.schemeCategory10;
 
-  const parties = load_parties();
+  const parties = load_parties(x_scale, y_scale);
+
+  // BaseType | SVGCircleElement
+  const drag = d3.drag<any, Circle>()
+    .on("start", drag_started)
+    .on("drag", dragging)
+    .on("end", drag_ended);
+
+  const svg_circle_element = "circle";
+
+  const p = parties
+    .map(({ x, y, color }) => ({ x: x_scale(x), y: y_scale(y), color }));
+
+  svg.selectAll("parties")
+    .data(p)
+    .join(svg_circle_element)
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y)
+    .attr("r", circle_radius)
+    .attr("fill", d => d.color)
+    .attr('class', 'party-circle')
+    .call(drag);
 
   const progress = document.querySelector('progress')
 
   const worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' });
 
   worker.onmessage = (msg: MessageEvent<{ answer: Simulation }>) => {
+    svg.selectAll(".points").remove();
     const r = msg.data.answer;
     const points = r.map(([voter_mean, seats_by_party]) => {
       const vx = x_scale(voter_mean[0]);
@@ -72,33 +108,14 @@ function main() {
       return { x: vx, y: vy, color };
     })
 
-    // BaseType | SVGCircleElement
-    const drag = d3.drag<any, Circle>()
-      .on("start", drag_started)
-      .on("drag", dragging)
-      .on("end", drag_ended);
-
-    const svg_circle_element = "circle";
-
-    const p = parties
-      .map(({ x, y, color }) => ({ x: x_scale(x), y: y_scale(y), color }));
-
-    svg.selectAll("parties")
-      .data(p)
-      .join(svg_circle_element)
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y)
-      .attr("r", circle_radius)
-      .attr("fill", d => d.color)
-      .call(drag);
-
     svg.selectAll("points")
       .data(points)
       .join(svg_circle_element)
       .attr("cx", d => d.x)
       .attr("cy", d => d.y)
       .attr("r", 1)
-      .attr("fill", d => d.color);
+      .attr("fill", d => d.color)
+      .attr('class', 'points');
 
     if (progress) {
       progress.value = 0;
@@ -110,7 +127,9 @@ function main() {
     if (progress) {
       progress.removeAttribute('value');
     }
-    worker.postMessage({ parties: parties });
+    worker.postMessage({
+      parties: load_parties(x_scale, y_scale)
+    });
   })
 }
 
