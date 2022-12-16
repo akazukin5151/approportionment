c447049b066be5787723f518ed93e15d358b08f3
akazukin5151 <tsuiyikching@protonmail.com>
2022-12-16 13:56:24 +0800

    Use colors from dhall

--- typescript/src/utils.ts
+++ typescript/src/utils.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_PARTIES = [
-  { x: -0.7, y: 0.7, color: 'red' },
-  { x: 0.7, y: 0.7, color: 'red' },
-  { x: 0.7, y: -0.7, color: 'red' },
-  { x: -0.7, y: -0.7, color: 'red' },
+  { x: -0.7, y: 0.7, color: '#F44336' },
+  { x: 0.7, y: 0.7, color: '#2196F3' },
+  { x: 0.7, y: -0.7, color: '#4CAF50' },
+  { x: -0.7, y: -0.7, color: '#FF9800' },
 ]
 
