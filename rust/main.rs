6577ba27498071204f40093330efd677ba525cbd
akazukin5151 <tsuiyikching@protonmail.com>
2022-12-30 16:09:06 +0000

    Calculate total_ballots only if progress bar enabled

--- rust/main.rs
+++ rust/main.rs
@@ -45,32 +45,31 @@ fn main() {
     });
     let configs = c.configs;
 
-    let total_ballots: u64 = configs
-        .iter()
-        .map(|c| {
-            let mut ballots_in_config: u64 = 0;
-            let out_dir = &c.data_out_dir;
-            let path = Path::new(&out_dir);
-            for method in &c.allocation_methods {
-                let filename = path.join(method.filename());
-                if !filename.exists() {
-                    let n_voters = c.n_voters as u64;
-                    // if domain is customizable this will change
-                    // there are 200 values between -100 to 100
-                    let n_coords = 200 * 200;
-                    let r: u64 = n_voters
-                        .checked_mul(n_coords)
-                        .expect("Overflow in mul");
-                    ballots_in_config = ballots_in_config
-                        .checked_add(r)
-                        .expect("Overflow in add");
-                }
-            }
-            ballots_in_config
-        })
-        .sum();
-
     let bar = if c.show_progress_bar {
+        let total_ballots: u64 = configs
+            .iter()
+            .map(|c| {
+                let mut ballots_in_config: u64 = 0;
+                let out_dir = &c.data_out_dir;
+                let path = Path::new(&out_dir);
+                for method in &c.allocation_methods {
+                    let filename = path.join(method.filename());
+                    if !filename.exists() {
+                        let n_voters = c.n_voters as u64;
+                        // if domain is customizable this will change
+                        // there are 200 values between -100 to 100
+                        let n_coords = 200 * 200;
+                        let r: u64 = n_voters
+                            .checked_mul(n_coords)
+                            .expect("Overflow in mul");
+                        ballots_in_config = ballots_in_config
+                            .checked_add(r)
+                            .expect("Overflow in add");
+                    }
+                }
+                ballots_in_config
+            })
+            .sum();
         Some(ProgressBar::new(total_ballots))
     } else {
         None
