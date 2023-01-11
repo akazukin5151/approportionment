a93d57591ac7129ed20f83e4db674556696104c3
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-11 15:19:55 +0000

    Use random_int instead of manually flooring

--- webui/typescript/random.ts
+++ webui/typescript/random.ts
@@ -7,17 +7,15 @@ export function round_1dp(n: number): number {
 }
 
 export function random_color(): string {
-  const r = Math.floor(random_between(0, 255))
-  const g = Math.floor(random_between(0, 255))
-  const b = Math.floor(random_between(0, 255))
+  const r = random_int(0, 255)
+  const g = random_int(0, 255)
+  const b = random_int(0, 255)
   // this function is only used to add a value for the color picker input,
   // which only accepts hexadecimal colors
   return '#' + r.toString(16) + g.toString(16) + b.toString(16)
 }
 
 export function random_int(min: number, max: number): number {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(random_between(min, max))
 }
 
