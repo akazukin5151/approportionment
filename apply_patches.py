11afb5edbc194cf38ade10bc8d10a44032b7792e
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-12 11:26:18 +0000

    Remove unused python scripts used for CI benchmarking

--- apply_patches.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.chdir('rust/stv')
-os.system('patch -u lib.rs -i restore.patch')
-
-for n in [1, 2, 3, 4]:
-    if os.getenv('p' + str(n)) == 'true':
-        os.system(f'patch -u lib.rs -i {n}.patch -f')
-
--- get_num.py
+++
@@ -1,9 +0,0 @@
-import os
-
-s = ''
-for n in [1, 2, 3, 4]:
-    if os.getenv('p' + str(n)) == 'true':
-        s += '1'
-    else:
-        s += '0'
-print(s)
