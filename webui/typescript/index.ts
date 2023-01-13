a49d6d9e0869805ac9e0b3485ae1c8f122ddaa82
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-13 16:25:26 +0000

    Rename setup_page to setup_colorscheme_select

--- webui/typescript/index.ts
+++ webui/typescript/index.ts
@@ -1,6 +1,6 @@
 import { setup_form_handler } from './setup/setup_form'
 import { setup_worker } from './setup/setup_worker'
-import { load_cmaps } from './setup/setup_page'
+import { load_cmaps } from './setup/setup_colorscheme_select'
 import { setup_party_table } from './party_table/setup_party_table';
 import { setup_coalition_table } from './coalition_table/setup_coalition_table';
 import { plot_party_with_listeners } from './plot/party/plot_party';
