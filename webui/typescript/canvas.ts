008691e21f24d09d3c38220dbe90c002098f1a4b
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-13 12:23:02 +0000

    Turn cursor into crosshair when hovering over canvas

--- webui/typescript/canvas.ts
+++ webui/typescript/canvas.ts
@@ -5,6 +5,8 @@ export function setup_canvas(z_index: number, chart: HTMLElement): Canvas {
   elem.width = 200
   elem.height = 200
   elem.style.zIndex = z_index.toString()
+  elem.onmouseenter = () => document.body.style.cursor = 'crosshair'
+  elem.onmouseleave = () => document.body.style.cursor = 'default'
   chart.appendChild(elem)
   const ctx = elem.getContext('2d')!
   return { ctx, elem }
