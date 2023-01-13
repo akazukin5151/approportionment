f6294bdc1522c0b10945df943fa7d93397b14dea
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-13 16:21:46 +0000

    Replace var with const

--- webui/typescript/setup/setup_export_btn.ts
+++ webui/typescript/setup/setup_export_btn.ts
@@ -8,7 +8,7 @@ export function setup_export_button() {
     }
     const j = JSON.stringify(cache)
     const str = "data:text/json;charset=utf-8," + encodeURIComponent(j)
-    var a = document.createElement('a');
+    const a = document.createElement('a');
     a.setAttribute("href", str);
     a.setAttribute("download", "simulation_result.json");
     document.body.appendChild(a);
