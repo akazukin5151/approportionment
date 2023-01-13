174f25766fbdd7e34265875297972b9bdc78db7a
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-13 12:15:59 +0000

    Ignore export button click if cache is null

--- webui/typescript/setup/setup_export_btn.ts
+++ webui/typescript/setup/setup_export_btn.ts
@@ -3,6 +3,9 @@ import { cache } from "./setup_worker";
 export function setup_export_button() {
   const btn = document.getElementById('export-btn')!;
   btn.addEventListener('click', () => {
+    if (!cache) {
+      return
+    }
     const j = JSON.stringify(cache)
     const str = "data:text/json;charset=utf-8," + encodeURIComponent(j)
     var a = document.createElement('a');
