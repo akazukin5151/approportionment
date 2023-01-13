95a9572a4dc1af8e7e603af5d3a7c2a10a6fb0b6
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-13 10:44:16 +0000

    Split up large main function in worker to smaller ones

--- webui/typescript/worker.ts
+++ webui/typescript/worker.ts
@@ -2,39 +2,40 @@ import init, { run, run_2 } from "libapproportionment";
 import { SimulationResults, SimulationResult, WasmRunArgs } from './types';
 
 function main(evt: MessageEvent<WasmRunArgs>): void {
-  const parties = evt.data.parties;
-  const method = evt.data.method;
-  const n_seats = evt.data.n_seats;
-  const n_voters = evt.data.n_voters;
   init().then(() => {
     if (evt.data.real_time_progress_bar) {
-      let counter = 1
-      for (let x = -100; x < 100; x++) {
-        for (let y = 100; y > -100; y--) {
-          try {
-            const single_answer: SimulationResult =
-              run_2(method, n_seats, n_voters, parties, x / 100, y / 100)
-            self.postMessage({ single_answer, counter })
-          } catch (e) {
-            self.postMessage({ error: e });
-            return
-          }
-          counter += 1
-        }
-      }
+      run_with_progress(evt.data)
     } else {
-      let r: SimulationResults | null = null;
+      run_without_progress(evt.data)
+    }
+  });
+}
+
+function run_with_progress({method, n_seats, n_voters, parties}: WasmRunArgs) {
+  let counter = 1
+  for (let x = -100; x < 100; x++) {
+    for (let y = 100; y > -100; y--) {
       try {
-        r = run(method, n_seats, n_voters, parties);
+        const single_answer: SimulationResult =
+          run_2(method, n_seats, n_voters, parties, x / 100, y / 100)
+        self.postMessage({ single_answer, counter })
       } catch (e) {
         self.postMessage({ error: e });
         return
       }
-      if (r) {
-        self.postMessage({ answer: r })
-      }
+      counter += 1
     }
-  });
+  }
+}
+
+function run_without_progress({method, n_seats, n_voters, parties}: WasmRunArgs) {
+  try {
+    const r: SimulationResults = run(method, n_seats, n_voters, parties);
+    self.postMessage({ answer: r })
+  } catch (e) {
+    self.postMessage({ error: e });
+    return
+  }
 }
 
 self.onmessage = main
