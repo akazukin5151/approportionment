55ee4a8b9ac3435327eb4a0824a78ee18fe7f6a8
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-15 17:45:20 +0000

    Add legend table next to coalition table (with inaccurate headers)

--- webui/typescript/plot/plot_simulation.ts
+++ webui/typescript/plot/plot_simulation.ts
@@ -1,16 +1,19 @@
 import { plot_colors_to_canvas } from '../canvas';
 import { SimulationPoint, SimulationResults, Canvas } from '../types';
-import { calculate_color } from '../plot_utils';
+import { calculate_cache_and_legend, rebuild_legend } from '../plot_utils';
 
 export function plot_simulation(
   canvas: Canvas,
   r: SimulationResults
 ): Array<SimulationPoint> {
-  const points = calculate_color(r)
+  const x = calculate_cache_and_legend(r)
+  const points = x.new_cache
 
   // informal timings suggests that this is extremely fast already
   // so there's no need to use wasm to fill in the image data array
   plot_colors_to_canvas(canvas, 0, points.map(p => p.color))
+  // TODO: merge with replot
+  rebuild_legend(x.legend)
 
   return points
 }
--- webui/typescript/plot_utils.ts
+++ webui/typescript/plot_utils.ts
@@ -5,18 +5,22 @@ import { Colormap } from "./colormap"
 import { array_max, array_sum, parties_equals } from "./std_lib"
 import { Canvas, Rgb, SimulationPoint, SimulationResult, SimulationResults } from "./types"
 import { map_to_lch, transform_to_radial } from "./colormap_nd"
+import { create_text_td } from "./td"
 
 export function replot(simulation_canvas: Canvas): void {
   const parties = load_parties()
   if (cache && parties_equals(cache.parties, parties)) {
-    const new_cache = calculate_color(cache.cache)
+    const { new_cache, legend } = calculate_cache_and_legend(cache.cache)
     set_cache({ cache: new_cache, parties })
     clear_canvas(simulation_canvas)
     plot_colors_to_canvas(simulation_canvas, 0, new_cache.map(p => p.color))
+    rebuild_legend(legend)
   }
 }
 
-export function calculate_color(r: SimulationResults): Array<SimulationPoint> {
+export function calculate_cache_and_legend(
+  r: SimulationResults
+): { new_cache: Array<SimulationPoint>; legend: Rgb[] } {
   // TODO: copied from Colormap
   const selector = document.getElementById('cmap_select')!
   const colormap_nd = selector.children[2]!
@@ -27,8 +31,8 @@ export function calculate_color(r: SimulationResults): Array<SimulationPoint> {
     const radviz = transform_to_radial(r.map(x => x.seats_by_party))
     const colors = map_to_lch(radviz.seat_coords)
     const legend = map_to_lch(radviz.party_coords)
-    console.log(legend)
-    return r.map((x, i) => ({ ...x, color: colors[i]! }))
+    const new_cache = r.map((x, i) => ({ ...x, color: colors[i]! }))
+    return { new_cache, legend }
   } else {
     const max_seats = array_max(r.map(x => x.seats_by_party.length))
     // TODO: duplicated code
@@ -37,8 +41,8 @@ export function calculate_color(r: SimulationResults): Array<SimulationPoint> {
     for (let i = 0; i < max_seats; i++) {
       legend.push(cmap.map(i, max_seats))
     }
-    console.log(legend)
-    return r.map(map_seats_to_cmap)
+    const new_cache = r.map(map_seats_to_cmap)
+    return { new_cache, legend }
   }
 }
 
@@ -59,5 +63,29 @@ function get_party_to_colorize(): number {
     .map((elem, idx) => ({ elem, idx }))
     .find(({ elem }) => (elem as HTMLInputElement).checked);
   return checked?.idx ?? 2
+}
+
+export function rebuild_legend(legend: Array<Rgb>) {
+  const table = document.getElementById('legend-table')!
+  const tbody = table.children[1]!
+
+  while (tbody.lastChild) {
+    tbody.removeChild(tbody.lastChild)
+  }
+
+  legend.forEach((color, idx) => {
+    const tr = document.createElement('tr')
+
+    const color_td = document.createElement('td')
+    const square = document.createElement('div')
+    square.style.width = '20px'
+    square.style.height = '20px'
+    square.style.backgroundColor = color.toString()
+    color_td.appendChild(square)
+    tr.appendChild(color_td)
+
+    tr.appendChild(create_text_td(idx))
+    tbody.appendChild(tr)
+  })
 }
 
