53bfd1c33f5be02a1a8f1f89b1727e8a497b294f
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-19 22:14:02 +0000

    Add search for unneeded export of let variables

--- webui/find-unneeded-export.sh
+++ webui/find-unneeded-export.sh
@@ -4,3 +4,10 @@ for f in $(rg 'export function (.*?)[<\(]' -r '$1' -o -N -I); do
         rg "$f"
     fi
 done
+
+for f in $(rg 'export let (.*?)[=:]' -r '$1' -o -N -I -t ts); do
+    s=$(rg "$f" -t ts --count-matches -I | awk '{if($0!=0) s+=$1}END{print s}')
+    if [[ "$s" -eq 2 ]]; then
+        rg "$f"
+    fi
+done
