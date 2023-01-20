6c45e48097240510838ba18308e2269ab310c0f4
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-20 11:12:32 +0000

    Refactor common code from color_wheel/drag.ts into drag.ts

--- webui/typescript/color_wheel/drag.ts
+++ webui/typescript/color_wheel/drag.ts
@@ -1,9 +1,8 @@
-// TODO: copied from plot/party
-
 import { clear_canvas } from "../canvas"
 import { angle_of_point } from "../colormap_nd/angle"
 import { pointer_pct_to_grid, pointer_to_pct } from "../convert_locations"
-import { Dimension, GridCoords } from "../types"
+import { abstract_on_drag_move, abstract_on_drag_start } from "../drag"
+import { GridCoords } from "../types"
 
 let dragging: number | null = null
 
@@ -14,14 +13,7 @@ export function on_drag_start(
   plotter: (angle: number) => void
 ): void {
   const l = (e: Event): void => on_drag_move(ctx, e, plotter, party_coords)
-  event.target!.addEventListener('mousemove', l)
-  event.target!.addEventListener('mouseup', (evt) => {
-    evt.target!.removeEventListener('mousemove', l)
-    dragging = null
-    if (document.body.style.cursor === 'grabbing') {
-      document.body.style.cursor = 'grab'
-    }
-  })
+  abstract_on_drag_start(event, l, () => dragging = null)
 }
 
 function on_drag_move(
@@ -30,26 +22,29 @@ function on_drag_move(
   plotter: (angle: number) => void,
   party_coords: Array<GridCoords>,
 ): void {
-  const evt = event as MouseEvent
-  const pointer_coords = pointer_pct_to_grid(pointer_to_pct(evt))
-  if (dragging === null) {
-    dragging = find_hovered_party(pointer_coords, party_coords)
-  }
+  abstract_on_drag_move(
+    event,
+    (evt) => {
+      const pointer_coords = pointer_pct_to_grid(pointer_to_pct(evt))
+      dragging = find_hovered_party(pointer_coords, party_coords)
+    },
+    () => dragging,
+    (evt) => {
+      const pointer_coords = pointer_pct_to_grid(pointer_to_pct(evt))
+      document.body.style.cursor = 'grabbing'
+      const angle_of_pointer = angle_of_point(pointer_coords)
+      // we want to get the angle of the first party dot
+      // the angle of this party is also the angle from the first party to
+      // this party
+      // so the angle of the pointer is made up of the angle of the first party
+      // plus the angle from the first party to this party
+      const angle_of_this_party = (dragging! / party_coords.length) * (2 * Math.PI)
+      const angle_of_first_party = angle_of_pointer - angle_of_this_party
 
-  if (dragging !== null) {
-    document.body.style.cursor = 'grabbing'
-    const angle_of_pointer = angle_of_point(pointer_coords)
-    // we want to get the angle of the first party dot
-    // the angle of this party is also the angle from the first party to
-    // this party
-    // so the angle of the pointer is made up of the angle of the first party
-    // plus the angle from the first party to this party
-    const angle_of_this_party = (dragging / party_coords.length) * (2 * Math.PI)
-    const angle_of_first_party = angle_of_pointer - angle_of_this_party
-
-    clear_canvas(ctx)
-    plotter(angle_of_first_party)
-  }
+      clear_canvas(ctx)
+      plotter(angle_of_first_party)
+    }
+  )
 
 }
 
@@ -68,11 +63,5 @@ function find_hovered_party(
     })
     ?.i
     ?? null
-}
-
-// Different canvas
-function get_canvas_dimensions(): Dimension {
-  const canvas = document.getElementById('color-wheel')!
-  return { width: canvas.clientWidth, height: canvas.clientHeight }
 }
 
