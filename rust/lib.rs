fe71f92b85740d9f941837d72a21d6f6b4ec1de7
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-22 16:43:05 +0000

    Split out methods.rs from config.rs

--- rust/lib.rs
+++ rust/lib.rs
@@ -5,6 +5,7 @@ pub mod simulator;
 pub mod stv;
 pub mod types;
 pub mod utils;
+pub mod methods;
 
 #[cfg(feature = "wasm")]
 pub mod wasm;
@@ -19,6 +20,7 @@ pub use largest_remainder::*;
 pub use stv::*;
 pub use types::*;
 pub use utils::*;
+pub use methods::*;
 
 #[cfg(feature = "wasm")]
 pub use wasm::*;
