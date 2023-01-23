9a386112401334eb6d3292d5c3f10eadb8cc8f95
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-23 10:17:41 +0000

    Use sass to minify the css

--- webui/build.sh
+++ webui/build.sh
@@ -1,6 +1,7 @@
 MODE=${1:-production}
 npx webpack --mode=$MODE
 cp html/index.html dist/
-cp css/styles.css dist/
-cp css/vendor/*.css dist/
+sass css/styles.css dist/styles.css --style=compressed --no-source-map
+sass css/vendor/simple.css dist/simple.css --style=compressed --no-source-map
+cp css/vendor/github-corner.css dist/
 cp static/default_simulation_result.json dist/
