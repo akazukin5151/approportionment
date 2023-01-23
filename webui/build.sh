b299f2220e24590067e8bb098ad2cb3215f13f2b
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-23 10:09:33 +0000

    Move vendor dir to be inside css dir

--- webui/build.sh
+++ webui/build.sh
@@ -2,5 +2,5 @@ MODE=${1:-production}
 npx webpack --mode=$MODE
 cp html/index.html dist/
 cp css/styles.css dist/
-cp vendor/*.css dist/
+cp css/vendor/*.css dist/
 cp static/default_simulation_result.json dist/
