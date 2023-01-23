16d5f602c6294988d5f5f7081c3ae8f2a3ac59d8
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-23 10:44:20 +0000

    Split up css into smaller files

--- webui/build.sh
+++ webui/build.sh
@@ -1,7 +1,8 @@
+set -e
 MODE=${1:-production}
 npx webpack --mode=$MODE
 cp html/index.html dist/
-sass css/styles.css dist/styles.css --style=compressed --no-source-map
+sass css/styles.scss dist/styles.css --style=compressed --no-source-map
 sass css/vendor/simple.css dist/simple.css --style=compressed --no-source-map
 cp css/vendor/github-corner.css dist/
 cp static/default_simulation_result.json dist/
