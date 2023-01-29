b7d19abbd4e5701599240122f5e47221291e5173
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-29 14:58:09 +0000

    Remove parallelism in generate_stv_ballots -- no significant improvement

--- rust/stv/lib.rs
+++ rust/stv/lib.rs
@@ -1,41 +1,6 @@
-#[cfg(not(feature = "wasm"))]
-use rayon::prelude::*;
-
 use crate::*;
 use crate::stv::types::StvBallot;
 
-#[cfg(not(feature = "wasm"))]
-pub fn generate_stv_ballots(
-    voters: &[Voter],
-    parties: &[Party],
-    bar: &Option<ProgressBar>,
-) -> Vec<StvBallot> {
-    voters
-        .par_iter()
-        .map(|voter| {
-            if let Some(b) = bar {
-                b.inc(1);
-            }
-            let mut distances: Vec<_> = parties
-                .iter()
-                .enumerate()
-                .map(|(idx, party)| {
-                    let a = (party.x - voter.x).powi(2);
-                    let b = (party.y - voter.y).powi(2);
-                    (idx, (a + b).powf(0.5))
-                })
-                .collect();
-            distances.sort_unstable_by(|(_, a), (_, b)| {
-                a.partial_cmp(b).expect("partial_cmp found NaN")
-            });
-            let ballot: Vec<_> =
-                distances.iter().map(|(i, _)| *i).collect();
-            StvBallot(ballot)
-        })
-        .collect()
-}
-
-#[cfg(feature = "wasm")]
 pub fn generate_stv_ballots(
     voters: &[Voter],
     parties: &[Party],
