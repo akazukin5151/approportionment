408a343b0f5eeb6aa321f4130f5e101550f3da24
akazukin5151 <tsuiyikching@protonmail.com>
2023-01-31 22:13:21 +0000

    Add documentation for Allocate::new

--- rust/allocate.rs
+++ rust/allocate.rs
@@ -5,6 +5,10 @@ use rand::seq::SliceRandom;
 
 /// A process that can allocate decimal resources into integer seats
 pub trait Allocate {
+    /// The struct should store the ballots vector, which is reused
+    /// for every simulation
+    /// Initially it is empty, `self.generate_ballots` must be called to populate
+    /// the vec. (In tests it can be set directly)
     fn new(n_voters: usize) -> Self
     where
         Self: Sized;
