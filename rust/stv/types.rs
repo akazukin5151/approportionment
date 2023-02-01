3ef1751461a8a864f3a0ec13c33fb9ce44435e8a
akazukin5151 <tsuiyikching@protonmail.com>
2023-02-01 10:59:18 +0000

    Add comment explaining why StvBallot derives Clone

--- rust/stv/types.rs
+++ rust/stv/types.rs
@@ -1,3 +1,5 @@
 /// Vector of candidate idxes in order of first to last preference
+// Clone is needed for the new function to duplicate empty ballots
+// at the start
 #[derive(Clone, Debug, Default)]
 pub struct StvBallot(pub Vec<usize>);
