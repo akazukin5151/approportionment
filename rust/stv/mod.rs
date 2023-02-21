e8cb707d4472d13ef49500e7e3664736e7ba5f7a
akazukin5151 <tsuiyikching@protonmail.com>
2023-02-21 23:08:08 +0000

    Remove unused modules

--- rust/stv/mod.rs
+++ rust/stv/mod.rs
@@ -1,14 +1,10 @@
 mod australia;
 mod bitfields;
-mod utils;
-mod types;
 mod generate_ballots;
 mod core;
 
 pub use australia::*;
 pub use bitfields::*;
-pub use utils::*;
-pub use types::*;
 pub use generate_ballots::*;
 pub use self::core::allocate_seats_stv;
 
