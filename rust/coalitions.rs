4c2707392257afa5c54208ad542c441dafa4c4a2
akazukin5151 <tsuiyikching@protonmail.com>
2023-02-28 02:41:43 +0000

    Fix incorrect number of parties calculation for party discipline

--- rust/coalitions.rs
+++ rust/coalitions.rs
@@ -1,4 +1,5 @@
 use crate::Party;
+use std::collections::HashSet;
 
 // party_of_cands is a lookup table where the index is the cand_idx,
 // and the value is the party_idx
@@ -26,7 +27,11 @@ pub fn extract_stv_parties(candidates: &[Party]) -> (Vec<usize>, usize) {
     let party_of_cands: Vec<_> =
         parties.iter().map(|x| x.unwrap()).collect();
 
-    let n_parties = party_of_cands.len();
+    let mut unique_parties = HashSet::new();
+    for p in &party_of_cands {
+        unique_parties.insert(p);
+    }
+    let n_parties = unique_parties.len();
 
     (party_of_cands, n_parties)
 }
