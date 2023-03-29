ab59bfa67982ec5429b21e5221b97a36d3986b8d
akazukin5151 <tsuiyikching@protonmail.com>
2023-03-29 12:07:27 +0100

    test(stv): add support for exhausted ballots

--- rust/stv/tests/common_unit.rs
+++ rust/stv/tests/common_unit.rs
@@ -4,32 +4,22 @@
 use crate::stv::*;
 use crate::*;
 
-// some ranks in the ballots are irrelevant to the tests,
-// but must be in the ballots to satisfy the invariant property:
-// the ballots vec represents `n_candidates` columns and `n_voters` rows.
-// this forces all voters to rank all candidates, which is what the simulation does;
-// real world elections might not require this, but this is out of scope
-//
-// the tests indicates which values are irrelevant like this:
-// [1, 0, /**/ 2, 3, 4]
-// the values on the left of the comment are relevant,
-// the values on the right of the comment are irrelevant padding
-
+// source:
 // https://www.aec.gov.au/learn/files/poster-count-senate-pref-voting.pdf
 #[test]
 fn stv_australia_pdf() {
     // Voters with Kim as first preference
     // Total of 1250, which is Kim's first preference votes
     let mut ballots = vec![];
-    ballots.extend([1, 0, /**/ 2, 3, 4].repeat(400));
-    ballots.extend([1, 2, /**/ 0, 3, 4].repeat(150));
-    ballots.extend([1, 3, /**/ 0, 2, 4].repeat(500));
-    ballots.extend([1, 4, /**/ 0, 2, 3].repeat(200));
+    ballots.extend([1, 0, 5, 5, 5].repeat(400));
+    ballots.extend([1, 2, 5, 5, 5].repeat(150));
+    ballots.extend([1, 3, 5, 5, 5].repeat(500));
+    ballots.extend([1, 4, 5, 5, 5].repeat(200));
 
-    ballots.extend([0, /**/ 1, 2, 3, 4].repeat(200));
-    ballots.extend([2, /**/ 0, 1, 3, 4].repeat(350));
-    ballots.extend([3, /**/ 0, 1, 2, 4].repeat(950));
-    ballots.extend([4, /**/ 0, 1, 2, 3].repeat(250));
+    ballots.extend([0, 5, 5, 5, 5].repeat(200));
+    ballots.extend([2, 5, 5, 5, 5].repeat(350));
+    ballots.extend([3, 5, 5, 5, 5].repeat(950));
+    ballots.extend([4, 5, 5, 5, 5].repeat(250));
 
     let total_seats = 2;
     let n_candidates = 5;
@@ -49,20 +39,16 @@ fn stv_australia_pdf() {
 }
 
 // https://en.wikipedia.org/wiki/Single_transferable_vote#Example_for_a_non-partisan_election
-// originally this had exhausted votes, but the flattened ballots forced
-// voters to fill out their entire ballot. I filled them out in no particular order,
-// just to satisfy the new constraint. The new ranks changes the results,
-// but it is correct for these set of ballots
 #[test]
 fn stv_australia_food() {
     let mut ballots = vec![];
-    ballots.extend([0, 1, 2, 3, 4, 5, 6].repeat(4));
-    ballots.extend([1, 2, 3, 0, 4, 5, 6].repeat(7));
-    ballots.extend([2, 3, 1, 0, 4, 5, 6]);
-    ballots.extend([3, 4, 2, 0, 1, 5, 6].repeat(3));
-    ballots.extend([4, 3, 5, 0, 1, 2, 6]);
-    ballots.extend([5, 0, 1, 2, 3, 4, 6].repeat(4));
-    ballots.extend([6, 5, 0, 1, 2, 3, 4].repeat(3));
+    ballots.extend([0, 1, 7, 7, 7, 7, 7].repeat(4));
+    ballots.extend([1, 2, 3, 7, 7, 7, 7].repeat(7));
+    ballots.extend([2, 3, 1, 7, 7, 7, 7]);
+    ballots.extend([3, 4, 2, 7, 7, 7, 7].repeat(3));
+    ballots.extend([4, 3, 5, 7, 7, 7, 7]);
+    ballots.extend([5, 7, 7, 7, 7, 7, 7].repeat(4));
+    ballots.extend([6, 5, 7, 7, 7, 7, 7].repeat(3));
 
     let total_seats = 3;
     let n_candidates = 7;
--- rust/stv/utils.rs
+++ rust/stv/utils.rs
@@ -1,6 +1,27 @@
 use crate::*;
 
 /// O(v*p) - len of ballots is v*p
+//
+// some ranks in the ballots are irrelevant to the tests,
+// but must be in the ballots to satisfy the invariant property:
+// the ballots vec represents `n_candidates` columns and `n_voters` rows.
+//
+// we use a convention to indicate an exhausted ballot: place a value
+// greater than `n_candidates` to pad the voter rows.
+// Any such value represents an invalid candidate.
+// For example:
+// There are 5 candidates. This ballot ranked the second over the first, then
+// exhausted their ballot
+// [1, 0, 5, 5, 5]
+//
+// We have an entire range of usize, but only a small sub-range will be used,
+// so we (mis)use the rest of the otherwise unused range
+// to indicate either a valid rank or an exhausted ballot
+//
+// An enum can be used instead: `enum Rank { Cand(usize), Exhausted }`
+// or more correctly, the ballots vec should not be flattened
+// but in our simulation, we will never fill exhausted ballots,
+// this is only used in unit tests, so any performance slowdown is not worth it
 pub fn calc_votes_to_transfer(
     ballots: &[usize],
     result: &[Option<f32>],
@@ -19,6 +40,13 @@ pub fn calc_votes_to_transfer(
     let mut compounded_tv = 1.;
     while idx < ballots.len() {
         let cand = ballots[idx];
+        // ballot exhausted
+        if cand >= n_candidates {
+            idx = next_row_idx;
+            next_row_idx += n_candidates;
+            compounded_tv = 1.;
+            continue;
+        }
         // check if this is the first valid preference
         // (not elected and not eliminated)
         if let Some(tv) = result[cand] {
@@ -35,20 +63,32 @@ pub fn calc_votes_to_transfer(
             if cand == idx_to_search {
                 // find their next valid preference
                 for next_cand in &ballots[idx + 1..next_row_idx] {
-                    if result[*next_cand].is_none()
-                        && !is_nth_flag_set(eliminated, *next_cand)
-                        && !is_nth_flag_set(pending, *next_cand)
-                    {
-                        // transfer this voter's vote to the next valid preference
-                        votes_to_transfer[*next_cand] += compounded_tv;
-                        break;
+                    match result.get(*next_cand) {
+                        // this cand is not elected
+                        Some(None) => {
+                            // if this cand is also not eliminated or pending,
+                            // they are the next valid preference
+                            if !is_nth_flag_set(eliminated, *next_cand)
+                                && !is_nth_flag_set(pending, *next_cand)
+                            {
+                                // transfer this voter's vote to
+                                // the next valid preference
+                                votes_to_transfer[*next_cand] += compounded_tv;
+                                break;
+                            }
+                        }
+                        // this cand is elected so cannot be a valid preference
+                        Some(Some(_)) => {}
+                        // ballot exhausted, break to move on to next voter
+                        None => break,
                     }
                 }
             }
             // if this voter's first valid preference is not the one to search for,
             // there is nothing we need to do and we can move on to the next voter.
-            // if we have transferred here, this voter's ballot is also fixed
-            // and we can also move on to the next voter
+            // if we have transferred here, this voter's ballot has nothing else
+            // to do and we can also move on to the next voter
+            // if the ballot was exhausted, we also move on to the next voter
             idx = next_row_idx;
             next_row_idx += n_candidates;
             // reset the compounded transfer value for this voter
