3ca7739fb8f5dd0a70016373c4c7ee07474d42da
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-05 17:05:45 +0100

    refactor(benchmarks): move iai abstract benchmark function to separate module

--- benches/iai_bench/party_list.rs
+++ benches/iai_bench/party_list.rs
@@ -1,36 +1,10 @@
-use iai::black_box;
 use libapproportionment::{
     allocate::Allocate,
-    generators::generate_voters,
     highest_averages::{dhondt::DHondt, webster::WebsterSainteLague},
     largest_remainder::{droop::Droop, hare::Hare},
 };
 
-use super::super::parties::*;
-
-fn simple_benchmark(mut a: impl Allocate, n_voters: usize, total_seats: usize) {
-    let stdev = 1.0;
-    let voter_mean = (0., 0.);
-
-    let voters = black_box(generate_voters(
-        black_box(voter_mean),
-        black_box(n_voters),
-        black_box(stdev),
-    ));
-    a.generate_ballots(
-        black_box(&voters),
-        black_box(TRIANGLE_PARTIES),
-        #[cfg(feature = "stv_party_discipline")]
-        &vec![],
-        #[cfg(feature = "stv_party_discipline")]
-        0,
-    );
-    black_box(a.allocate_seats(
-        black_box(total_seats),
-        black_box(TRIANGLE_PARTIES.len()),
-        black_box(n_voters),
-    ));
-}
+use super::simple::simple_benchmark;
 
 macro_rules! make_bench {
     ($fn_name:ident, $name:ident, $n_voters:expr, $n_seats:expr) => {
---
+++ benches/iai_bench/simple.rs
@@ -0,0 +1,32 @@
+use iai::black_box;
+use libapproportionment::{
+    allocate::Allocate,
+    generators::generate_voters,
+};
+
+use super::super::parties::*;
+
+pub fn simple_benchmark(mut a: impl Allocate, n_voters: usize, total_seats: usize) {
+    let stdev = 1.0;
+    let voter_mean = (0., 0.);
+
+    let voters = black_box(generate_voters(
+        black_box(voter_mean),
+        black_box(n_voters),
+        black_box(stdev),
+    ));
+    a.generate_ballots(
+        black_box(&voters),
+        black_box(TRIANGLE_PARTIES),
+        #[cfg(feature = "stv_party_discipline")]
+        &vec![],
+        #[cfg(feature = "stv_party_discipline")]
+        0,
+    );
+    black_box(a.allocate_seats(
+        black_box(total_seats),
+        black_box(TRIANGLE_PARTIES.len()),
+        black_box(n_voters),
+    ));
+}
+
