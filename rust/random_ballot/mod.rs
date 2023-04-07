6a1d3cb84bfff15c0dcee7ecc3c2fa46afa38244
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-07 10:59:35 +0100

    format(random_ballot): use underscore for unused function params

--- rust/random_ballot/mod.rs
+++ rust/random_ballot/mod.rs
@@ -34,8 +34,8 @@ impl Allocate for RandomBallot {
         voters: &[XY],
         parties: &[Party],
         #[cfg(feature = "progress_bar")] bar: &ProgressBar,
-        #[cfg(feature = "stv_party_discipline")] party_of_cands: &[usize],
-        #[cfg(feature = "stv_party_discipline")] n_parties: usize,
+        #[cfg(feature = "stv_party_discipline")] _: &[usize],
+        #[cfg(feature = "stv_party_discipline")] _: usize,
     ) {
         generate_ballots(
             voters,
