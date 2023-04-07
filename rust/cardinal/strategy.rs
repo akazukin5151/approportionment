05988823d4fff9b51e5e1aa42564a0d4d8d668d8
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-07 12:26:40 +0100

    perf(cardinal): directly use quickselect instead of partial sort for median

--- rust/cardinal/strategy.rs
+++ rust/cardinal/strategy.rs
@@ -1,4 +1,5 @@
-use arrow::compute::partial_sort;
+use std::cmp::Ordering;
+
 use serde::Deserialize;
 
 pub trait Strategy {
@@ -22,7 +23,6 @@ pub enum CardinalStrategy {
     /// Distances are normalized (stretched from 0.0 to 1.0), and the
     /// intermediate values are transformed in a linear manner
     NormedLinear,
-
     // All voters will score candidates according to a fixed scale
     // The scale is a mapping of distances to scores
     // (NB: isn't a function better than a vec?)
@@ -64,24 +64,7 @@ fn median_strategy(dists: &[f32], result: &mut [f32]) {
     // copy
     let mut to_sort = vec![0.; dists.len()];
     to_sort.copy_from_slice(dists);
-    // we don't care about the remainder
-    #[allow(clippy::integer_division)]
-    let l = to_sort.len() / 2;
-    partial_sort(&mut to_sort, l, |a, b| {
-        a.partial_cmp(b).expect("partial_cmp found NaN")
-    });
-
-    let median = if to_sort.len() % 2 == 0 {
-        // we don't care about the remainder
-        #[allow(clippy::integer_division)]
-        let upper_mid = to_sort.len() / 2;
-        let lower_mid = upper_mid - 1;
-        (to_sort[lower_mid] + to_sort[upper_mid]) / 2.
-    } else {
-        // there's no rounding here so this is fine
-        #[allow(clippy::integer_division)]
-        to_sort[to_sort.len() / 2]
-    };
+    let median = quickselect_median(&mut to_sort);
 
     for (idx, dist) in dists.iter().enumerate() {
         if dist <= &median {
@@ -106,12 +89,33 @@ fn normed_linear(dists: &[f32], result: &mut [f32]) {
     }
 }
 
+#[inline(always)]
+fn f32_cmp(a: &f32, b: &f32) -> Ordering {
+    a.partial_cmp(b).expect("partial_cmp found NaN")
+}
+
+fn quickselect_median(arr: &mut [f32]) -> f32 {
+    // we don't care about the remainder
+    #[allow(clippy::integer_division)]
+    let l = arr.len() / 2;
+    if arr.len() % 2 == 1 {
+        *arr.select_nth_unstable_by(l, f32_cmp).1
+    } else {
+        let (_, lower_mid, end) = arr.select_nth_unstable_by(l - 1, f32_cmp);
+        let (_, upper_mid, _) = end.select_nth_unstable_by(0, f32_cmp);
+        (*lower_mid + *upper_mid) / 2.
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::ops::Range;
+
     use super::*;
+    use proptest::{collection::vec, prelude::*};
 
     #[test]
-    fn test_mean() {
+    fn test_mean_strategy() {
         let dists = [2.3, 1.5, 3.4, 2.9];
         let mut result = [0., 0., 0., 0.];
         mean_strategy(&dists, &mut result);
@@ -119,7 +123,7 @@ mod test {
     }
 
     #[test]
-    fn test_median() {
+    fn test_median_strategy() {
         let dists = [2.3, 1.5, 3.4, 2.9];
         let mut result = [0., 0., 0., 0.];
         median_strategy(&dists, &mut result);
@@ -132,5 +136,30 @@ mod test {
         let mut result = [0., 0., 0., 0.];
         normed_linear(&dists, &mut result);
         assert_eq!(result, [0.5789474, 1.0, 0.0, 0.2631579]);
+    }
+
+    proptest! {
+        #[test]
+        fn test_quickselect_median(
+            // median will crash if len is 0 (0 candidates)
+            // so ideally it should return an Option
+            // but this isn't important here so we can ignore it
+            mut v in vec(Range { start: 0.0_f32, end: 100.}, 1..100)
+        ) {
+            let mut cloned = v.clone();
+            cloned.sort_unstable_by(f32_cmp);
+            #[allow(clippy::integer_division)]
+            let upper_mid = cloned.len() / 2;
+            let slow_median = if v.len() % 2 == 0 {
+                let lower_mid = upper_mid - 1;
+                (cloned[lower_mid] + cloned[upper_mid]) / 2.
+            } else {
+                #[allow(clippy::integer_division)]
+                cloned[upper_mid]
+            };
+
+            let median = quickselect_median(&mut v);
+            assert_eq!(median, slow_median);
+        }
     }
 }
