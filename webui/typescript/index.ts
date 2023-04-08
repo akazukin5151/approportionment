8dad7a6d1503005ae3a3d4ddbb3bd53bcb58e94f
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-08 10:19:48 +0100

    refactor(webui): move `plot_default_result` to index.ts and rename

--- webui/typescript/index.ts
+++ webui/typescript/index.ts
@@ -8,12 +8,14 @@ import { load_parties } from './form';
 import { setup_all_canvases } from './canvas';
 import { setup_export_button } from './setup/setup_export_btn';
 import { preplot_all } from './color_wheel/preplot';
-import { plot_default_result } from './plot/default';
 import { setup_voronoi } from './setup/setup_voronoi';
 import { show_error_dialog } from './dom';
 import { ProgressBar } from './progress';
 import { setup_import_btn } from './setup/setup_import_btn';
 import { setup_example_button } from './setup/setup_example_btn';
+import { AllCanvases } from './types/canvas';
+import { Save } from './types/cache';
+import { import_json } from './import';
 
 function main(): void {
   const chart = document.getElementById('chart')!
@@ -34,7 +36,26 @@ function main(): void {
   setup_example_button(all_canvases, worker)
 
   preplot_all()
-  plot_default_result(all_canvases, worker)
+  import_default_example(all_canvases, worker)
+}
+
+function import_default_example(
+  all_canvases: AllCanvases,
+  worker: Worker
+): void {
+  fetch('./square.json')
+    .then((response) => response.json())
+    .then((cache: Save) => {
+      // we force the initial n_voters to 100, even though the default
+      // uses 1000 voters, because 100 voters is faster, making a better
+      // first experience on running. 1000 is used to make the initial
+      // plot look better
+      cache.n_voters = 100
+      // we also force the initial seed to -1 so people aren't stuck
+      // on one variation
+      cache.seed = -1
+      import_json(all_canvases, cache, worker)
+    })
 }
 
 try {
