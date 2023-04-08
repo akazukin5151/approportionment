e26eaf59170b3211c763033870f25dea9e9fd602
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-08 10:18:18 +0100

    feat(webui): set initial example seed to -1

--- webui/typescript/plot/default.ts
+++ webui/typescript/plot/default.ts
@@ -10,9 +10,13 @@ export function plot_default_result(
     .then((response) => response.json())
     .then((cache: Save) => {
       // we force the initial n_voters to 100, even though the default
-      // uses 1000 voters, because 100 voters is faster. 1000 is used to
-      // make the initial plot look better
+      // uses 1000 voters, because 100 voters is faster, making a better
+      // first experience on running. 1000 is used to make the initial
+      // plot look better
       cache.n_voters = 100
+      // we also force the initial seed to -1 so people aren't stuck
+      // on one variation
+      cache.seed = -1
       import_json(all_canvases, cache, worker)
     })
 }
