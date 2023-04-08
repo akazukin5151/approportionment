90f8f1f14e633e24e72e54cd66027ac0a80f47f9
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-08 18:59:47 +0100

    test(utils): add property test for count_freqs

--- rust/utils.rs
+++ rust/utils.rs
@@ -16,3 +16,36 @@ pub fn wasm_debug(s: &str) {
 #[cfg(not(feature = "wasm_debug"))]
 #[allow(dead_code)]
 pub fn wasm_debug(_: &str) {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::collection::vec;
+    use proptest::prelude::*;
+
+    fn make_ballot() -> impl Strategy<Value = (usize, Vec<usize>)> {
+        (1..1000_usize).prop_flat_map(|n_parties| {
+            (Just(n_parties), vec(0..n_parties, 1000))
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn test_vote_counter(
+            (n_parties, ballots) in make_ballot()
+        ) {
+            assert!(ballots.iter().any(|x| x < &n_parties));
+
+            let r = count_freqs(&ballots, n_parties);
+            for (idx, freq) in r.iter().enumerate() {
+                let mut count = 0;
+                for ballot in &ballots {
+                    if *ballot == idx {
+                        count += 1;
+                    }
+                }
+                assert_eq!(*freq, count);
+            }
+        }
+    }
+}
