bbcdfe617342bb0bf47d97a5e45433c347d28f50
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-21 16:25:08 +0100

    refactor(webui): split out setup_party_canvas.ts from popup.ts

--- webui/typescript/plot/party/popup.ts
+++ webui/typescript/plot/party/popup.ts
@@ -1,10 +1,8 @@
 import { AllCanvases } from "../../types/canvas";
 import { Party } from "../../types/election";
-import { on_pointer_move } from '../hover/hover'
-import { on_drag_start } from './drag'
 import { CANDIDATE_BASED_METHODS, PARTY_CANVAS_SIZE, PARTY_RADIUS } from "../../constants";
 import { hide_voter_canvas } from "./utils";
-import { get_canvas_dimensions, get_method } from "../../form";
+import { get_method } from "../../form";
 import { computePosition, arrow, flip, shift, ClientRectObject } from "@floating-ui/dom";
 import { clear_legend_highlight } from "../../td";
 import { cache, coalition_bar_chart, party_bar_chart, party_manager } from "../../cache";
@@ -16,70 +14,9 @@ import { plot_voronoi, voronoi_enabled } from "../../setup/setup_voronoi";
 import { plot_party_on_wheel } from "../../color_wheel/plot";
 import { plot_parties, plot_single_party } from "./plot_party";
 
-const DELTA = 6
-let start_x: number
-let start_y: number
 let current_party_num: number | null = null
 
-export function setup_party_canvas(
-  all_canvases: AllCanvases,
-  worker: Worker
-): void {
-  // this isn't party canvas but relies on current_party_num
-  const add_near_btn = document.getElementById('near-btn') as HTMLElement
-  add_near_btn.addEventListener('click', () => on_near_click(worker))
-
-  const { party, voter, simulation } = all_canvases
-  party.elem.addEventListener('mousemove',
-    e => on_pointer_move(simulation, voter, e)
-  )
-  party.elem.addEventListener(
-    'mousedown',
-    e => {
-      start_x = e.pageX
-      start_y = e.pageY
-      on_drag_start(all_canvases, e, plot_single_party)
-    }
-  )
-  const floating = document.getElementById('floating') as HTMLElement
-  floating.style.display = 'none'
-  party.elem.addEventListener(
-    'mouseup',
-    e => {
-      const diff_x = Math.abs(e.pageX - start_x);
-      const diff_y = Math.abs(e.pageY - start_y);
-
-      if (diff_x < DELTA && diff_y < DELTA) {
-        on_party_click(e, floating, all_canvases)
-      }
-    }
-  )
-  party.elem.addEventListener('mouseleave', () =>
-    hide_voter_canvas(all_canvases, voter)
-  )
-}
-
-function on_party_click(
-  e: Event,
-  floating: HTMLElement,
-  all_canvases: AllCanvases,
-): void {
-  const evt = e as MouseEvent
-  const p = party_manager.find_hovered_party(
-    evt.offsetX, evt.offsetY, get_canvas_dimensions()
-  )
-  if (!p) {
-    return
-  }
-  if (current_party_num === null || p.num === current_party_num) {
-    toggle_dropdown(floating, 'floating')
-  }
-  current_party_num = p.num
-
-  set_position(evt, floating, p, all_canvases)
-}
-
-function set_position(
+export function set_position(
   evt: MouseEvent,
   floating: HTMLElement,
   p: Party,
@@ -105,7 +42,7 @@ function set_position(
 }
 
 // TODO: copied from setup/dropdown.ts
-function toggle_dropdown(
+export function toggle_dropdown(
   dropdown: HTMLElement,
   dropdown_id: string
 ): void {
@@ -249,7 +186,7 @@ function setup_input(input: HTMLInputElement,
   })
 }
 
-function on_near_click(worker: Worker): void {
+export function on_near_click(worker: Worker): void {
   if (current_party_num == null) {
     return
   }
