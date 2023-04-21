75ec05dfa328db5868eefcf8b02a688fba56a500
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-21 17:15:47 +0100

    refactor(webui): move closures to functions to reduce parent function length

--- webui/typescript/coalition_table/drag_and_drop.ts
+++ webui/typescript/coalition_table/drag_and_drop.ts
@@ -17,46 +17,44 @@ export function add_drop_listeners(
   div: HTMLDivElement,
   simulation_canvas: Canvas
 ): void {
-  div.addEventListener(
-    'drop',
-    ev => {
-      ev.preventDefault()
-      ev.dataTransfer!.dropEffect = "move"
-      const elem_id = ev.dataTransfer!.getData("text/plain")
-      const party_num = parseInt(elem_id.slice('party-dot-'.length))
+  div.addEventListener('drop', ev => on_drop(ev, simulation_canvas))
+  div.addEventListener('dragover', on_drag_over)
+}
 
-      const party_dot_area = ev.target as HTMLElement
-      const col_2 = party_dot_area.parentElement
-      const col_1 = col_2?.previousElementSibling as HTMLElement
-      // the 'None' cell doesn't have a div around it
-      // coalition numbers have a div around them, but innerText works anyway
-      const n = col_1.innerText
-      const coalition_num = n === 'None' ? null : parseInt(n)
-      party_manager.coalitions.modify(party_num, coalition_num)
-      replot(simulation_canvas)
-    }
-  )
-  div.addEventListener(
-    'dragover',
-    ev => {
-      ev.preventDefault()
-      const elem = ev.target as HTMLElement
-      // prevent dropping into another party dot, overlapping them
-      if (elem.draggable) {
-        return
-      }
-      const data = ev.dataTransfer!.getData("text/plain")
-      elem.appendChild(document.getElementById(data)!)
+function on_drop(ev: DragEvent, simulation_canvas: Canvas): void {
+  ev.preventDefault()
+  ev.dataTransfer!.dropEffect = "move"
+  const elem_id = ev.dataTransfer!.getData("text/plain")
+  const party_num = parseInt(elem_id.slice('party-dot-'.length))
 
-      const party_num = parseInt(data.slice('party-dot-'.length))
-      const row = elem.parentElement!.parentElement!
-      const cnum_td = row.children[0] as HTMLElement
-      const n = cnum_td.innerText
-      const coalition_num = n === 'None' ? null : parseInt(n)
-      party_manager.coalitions.modify(party_num, coalition_num)
+  const party_dot_area = ev.target as HTMLElement
+  const col_2 = party_dot_area.parentElement
+  const col_1 = col_2?.previousElementSibling as HTMLElement
+  // the 'None' cell doesn't have a div around it
+  // coalition numbers have a div around them, but innerText works anyway
+  const n = col_1.innerText
+  const coalition_num = n === 'None' ? null : parseInt(n)
+  party_manager.coalitions.modify(party_num, coalition_num)
+  replot(simulation_canvas)
+}
 
-      // TODO replot if we are colorizing by coalition and that coalition has changed
-    }
-  )
+function on_drag_over(ev: DragEvent): void {
+  ev.preventDefault()
+  const elem = ev.target as HTMLElement
+  // prevent dropping into another party dot, overlapping them
+  if (elem.draggable) {
+    return
+  }
+  const data = ev.dataTransfer!.getData("text/plain")
+  elem.appendChild(document.getElementById(data)!)
+
+  const party_num = parseInt(data.slice('party-dot-'.length))
+  const row = elem.parentElement!.parentElement!
+  const cnum_td = row.children[0] as HTMLElement
+  const n = cnum_td.innerText
+  const coalition_num = n === 'None' ? null : parseInt(n)
+  party_manager.coalitions.modify(party_num, coalition_num)
+
+  // TODO replot if we are colorizing by coalition and that coalition has changed
 }
 
