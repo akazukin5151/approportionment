5e5ab58348a5b120478e329ed8170e40c5f93fdf
akazukin5151 <tsuiyikching@protonmail.com>
2023-04-22 22:57:38 +0100

    refactor(webui): move setup canvas functions to setup dir

--- webui/typescript/index.ts
+++ webui/typescript/index.ts
@@ -3,7 +3,7 @@ import { setup_worker } from './setup/setup_worker'
 import { setup_cmap_section } from './setup/setup_cmap_section'
 import { setup_coalition_table } from './coalition_table/setup_coalition_table';
 import { setup_party_canvas } from './setup/party_canvas/setup_party_canvas';
-import { setup_all_canvases } from './canvas';
+import { setup_all_canvases } from './setup/setup_canvas';
 import { setup_export_button } from './setup/setup_export_btn';
 import { preplot_all } from './color_wheel/preplot';
 import { setup_voronoi } from './setup/setup_voronoi';
