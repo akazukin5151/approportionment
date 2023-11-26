898f82d06cfcf9e51032f3b79adad1460e0f6d44
akazukin5151 <tsuiyikching@protonmail.com>
2023-11-26 22:39:26 +0800

    refactor: split up large function into smaller functions

--- webui/typescript/rounds.ts
+++ webui/typescript/rounds.ts
@@ -9,7 +9,12 @@ type ElectionResult = {
   rounds: Array<Array<number>>
 }
 
-type Selection = d3.Selection<any, [string, number], SVGElement, [string, number]>
+type Setup = {
+  all_rounds: Array<Map<string, number>>;
+  r1_sorted: Array<[string, number]>;
+}
+
+type Selection = d3.Selection<SVGGElement, [string, number], HTMLElement, any>
 
 type Rect = d3.Selection<SVGElement, [string, number], any, [string, number]>
 
@@ -52,24 +57,7 @@ export async function main(
   const slider = document.getElementsByClassName('rounds-slider')[0] as HTMLInputElement
   slider.value = '0'
 
-  const langs = await fetch(filename)
-  const json = await langs.json() as ElectionResult
-
-  const idx_to_id: Map<number, string> = new Map()
-  // for-in loop to loop over the fields
-  for (const x in json.choices) {
-    idx_to_id.set(json.choices[x]!, x)
-  }
-
-  const all_rounds = json.rounds.map(round => {
-    const map: Map<string, number> = new Map()
-    round.forEach((score, i) => {
-      map.set(idx_to_id.get(i)!, score)
-    })
-    return map
-  })
-
-  const r1_sorted = Array.from(all_rounds[0]!).sort((a, b) => b[1] - a[1])
+  const { all_rounds, r1_sorted } = await setup_data(filename)
 
   // set the dimensions and margins of the graph
   const margin = { top: 30, right: 30, bottom: 70, left: 150 },
@@ -82,8 +70,7 @@ export async function main(
     .attr("width", width + margin.left + margin.right)
     .attr("height", height_ + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")") as Selection;
 
   const y = d3.scaleBand()
     .range([0, height_])
@@ -103,108 +90,142 @@ export async function main(
   // Bars
   const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  function draw(data: Array<[string, number]>): Selection {
-    return svg.selectAll("bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", d => y(get_y_value(d))!)
-      .attr("height", y.bandwidth())
-      .attr("width", d => x(d[1]))
-  }
-
-  draw(r1_sorted)
-    .attr("fill", x => color(x[0]))
+  draw(svg, get_y_value, x, y, r1_sorted)
+    .attr("fill", d => color(d[0]))
     .attr('class', 'rect')
 
   slider.max = (json.rounds.length - 1).toString()
-  slider.oninput = (evt): void => {
-    diff.length = 0
-    const target = evt.target as HTMLInputElement
-    const round = parseInt(target.value)
-    const did_round_increase = round + 1 > parseInt(current_round.innerText)
-    current_round.innerText = (round + 1).toString()
+
+  function on_round_change(evt: Event): void {
+    diff.length = 0;
+    const target = evt.target as HTMLInputElement;
+    const round = parseInt(target.value);
+    const did_round_increase = round + 1 > parseInt(current_round.innerText);
+    current_round.innerText = (round + 1).toString();
 
     if (round >= 1) {
-      open_diff_btn.style.display = 'block'
+      open_diff_btn.style.display = 'block';
     } else {
-      open_diff_btn.style.display = 'none'
+      open_diff_btn.style.display = 'none';
     }
 
-    const selection = d3.selectAll(".rect") as Rect
+    const selection = d3.selectAll(".rect") as Rect;
     selection
       .transition()
       .attr("width", ([lang, initial], i) => {
-        const new_score = all_rounds[round]!.get(lang)
+        const new_score = all_rounds[round]!.get(lang);
         if (new_score == null || new_score === 0) {
           if (i === 0) {
-            return x(initial)
+            return x(initial);
           }
           for (let r = round - 1; r > 0; r--) {
-            const s = all_rounds[r]!.get(lang)
+            const s = all_rounds[r]!.get(lang);
             if (s != null && s !== 0) {
-              return x(s)
+              return x(s);
             }
           }
-          console.warn('null width')
-          return null
+          console.warn('null width');
+          return null;
         } else {
-          const prev_score = round !== 0 ? all_rounds[round - 1]!.get(lang)! : 0
-          const d = prev_score - new_score
-          diff.push([lang, d, new_score / prev_score])
-          return x(new_score)
+          const prev_score = round !== 0 ? all_rounds[round - 1]!.get(lang)! : 0;
+          const d = prev_score - new_score;
+          diff.push([lang, d, new_score / prev_score]);
+          return x(new_score);
         }
       })
       .attr("fill", ([lang, _], i) => {
-        const new_score = all_rounds[round]!.get(lang)
+        const new_score = all_rounds[round]!.get(lang);
         if (round !== 0) {
-          const prev_score = all_rounds[round - 1]!.get(lang)
+          const prev_score = all_rounds[round - 1]!.get(lang);
           if (round > 0 && (new_score == null || new_score === 0 || new_score === prev_score)) {
-            return 'none'
+            return 'none';
           }
         }
-        return d3.schemeCategory10[i % d3.schemeCategory10.length]!
+        return d3.schemeCategory10[i % d3.schemeCategory10.length]!;
       })
       .attr("stroke", ([lang, _], _i) => {
-        const new_score = all_rounds[round]!.get(lang)
+        const new_score = all_rounds[round]!.get(lang);
         if (round !== 0) {
-          const prev_score = all_rounds[round - 1]!.get(lang)
+          const prev_score = all_rounds[round - 1]!.get(lang);
           if (round > 0 && (new_score == null || new_score === 0 || new_score === prev_score)) {
-            return 'red'
+            return 'red';
           }
         }
-        return 'none'
-      })
+        return 'none';
+      });
 
-    svg.selectAll('.shadow').remove()
+    svg.selectAll('.shadow').remove();
 
     if (did_round_increase) {
       const r: Array<[string, number]> = r1_sorted.map(x => {
-        const lang = x[0]
-        return [lang, all_rounds[round - 1]!.get(lang)!]
-      })
+        const lang = x[0];
+        return [lang, all_rounds[round - 1]!.get(lang)!];
+      });
 
-      draw(r)
+      draw(svg, get_y_value, x, y, r)
         .attr("fill", 'none')
         .attr('stroke', ([lang, _], _i) => {
-          const new_score = all_rounds[round]!.get(lang)
-          const prev_score = all_rounds[round - 1]!.get(lang)
+          const new_score = all_rounds[round]!.get(lang);
+          const prev_score = all_rounds[round - 1]!.get(lang);
           if (new_score == null || new_score === 0 || new_score === prev_score) {
-            return 'none'
+            return 'none';
           } else {
-            return 'gray'
+            return 'gray';
           }
         })
-        .attr('class', 'shadow')
+        .attr('class', 'shadow');
     }
 
     if (diff_ui_container.style.display === "block") {
-      diff.sort((a, b) => a[2] - b[2])
-      draw_diff_chart(diff_chart, y_axis_domain, get_y_value, diff, diff_chart_height)
+      diff.sort((a, b) => a[2] - b[2]);
+      draw_diff_chart(diff_chart, y_axis_domain, get_y_value, diff, diff_chart_height);
     }
   }
+
+  slider.oninput = on_round_change
 }
+
+async function setup_data(
+  filename: string
+): Promise<Setup> {
+  const langs = await fetch(filename)
+  const json = await langs.json() as ElectionResult
+
+  const idx_to_id: Map<number, string> = new Map()
+  // for-in loop to loop over the fields
+  for (const x in json.choices) {
+    idx_to_id.set(json.choices[x]!, x)
+  }
+
+  const all_rounds = json.rounds.map(round => {
+    const map: Map<string, number> = new Map()
+    round.forEach((score, i) => {
+      map.set(idx_to_id.get(i)!, score)
+    })
+    return map
+  })
+
+  const r1_sorted = Array.from(all_rounds[0]!).sort((a, b) => b[1] - a[1])
+  return { all_rounds, r1_sorted }
+}
+
+function draw(
+  svg: Selection,
+  get_y_value: (candidate: [string, number]) => string,
+  x: d3.ScaleLinear<number, number, never>,
+  y: d3.ScaleBand<string>,
+  data: Array<[string, number]>
+): d3.Selection<SVGRectElement, [string, number], SVGGElement, [string, number]> {
+  return svg.selectAll("bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", d => y(get_y_value(d))!)
+    .attr("height", y.bandwidth())
+    .attr("width", d => x(d[1]))
+}
+
 
 function draw_diff_chart(
   diff_chart: Element,
